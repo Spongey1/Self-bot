205ef7b6976708e6cce68f63cf3beb296be21c25
Spongey One <Driton.latifi101@gmail.com>
2022-05-09 23:49:32 +0200

    .

--- bot.js
+++ bot.js
@@ -3,11 +3,6 @@ const config = require('./config.json')
 const Discord = require('discord.js-selfbot');
 const client = new Discord.Client();
 
-var job = new CronJob('0 */120 * * * *', function () {
-    const channel = client.channels.cache.get('814954977830371328');
-    console.log(channel.lastMessage)
-}, null, false)
-
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity("Sponge Killing Simulator");
@@ -15,7 +10,7 @@ client.once('ready', () => {
     job.start();
 })
 
-client.on('message', (msg) => { // 814954977830371328
+client.on('message', (msg) => {
     if (msg.channel.id == 814954977830371328 && msg.author.id != 883428081166061628 && isNaN(msg.content) == false) {
         msg.channel.send(parseInt(msg.content) + 1)
     }
